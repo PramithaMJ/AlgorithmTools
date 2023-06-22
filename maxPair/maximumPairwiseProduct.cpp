b0a569ddef32fe4feecb4565d45ef0e20238b8f8
PramithaMJ <lpramithamj@gmail.com>
2023-06-22 23:59:11 +0530

    first commit

--- maxPair/maximumPairwiseProduct.cpp
+++ maxPair/maximumPairwiseProduct.cpp
@@ -4,23 +4,29 @@
 
 using namespace std;
 
-int MaxPairwiseProduct(const std::vector<int> &numbers)
+long long MaxPairwiseProduct(const vector<int> &numbers)
 {
-    long long max_product = 0;
     int n = numbers.size();
 
+    int max_index1 = -1;
     for (int i = 0; i < n; ++i)
     {
-        for (int j = i + 1; j < n; ++j)
+        if (max_index1 == -1 || numbers[i] > numbers[max_index1])
         {
-            if ((numbers[i] * numbers[j]) > max_product)
-            {
-                max_product = (long long)(numbers[i] * numbers[j]);
-            }
+            max_index1 = i;
         }
     }
 
-    return max_product;
+    int max_index2 = -1;
+    for (int j = 0; j < n; ++j)
+    {
+        if (j != max_index1 && (max_index2 == -1 || numbers[j] > numbers[max_index2]))
+        {
+            max_index2 = j;
+        }
+    }
+
+    return (long long)numbers[max_index1] * numbers[max_index2];
 }
 
 int main()
